d391e2f2b83738aab9e859f898d6d63161b95840
Selwyn <talk@selwyn.cc>
2016-01-14 11:35:30 +0100

    Add DOM helper functions

---
+++ src/partials/core-helpers/dom.js
@@ -0,0 +1,7 @@
+var selectAttr = function (attrName) {
+  return document.querySelector('[' + attrName + ']');
+};
+
+var selectAttrAll = function (attrName) {
+  return document.querySelectorAll('[' + attrName + ']');
+};
