b82312fcc3df9c3392ad0a2bee6df62d65b8d6b7
Selwyn <talk@selwyn.cc>
2016-01-18 15:26:48 +0100

    Add map partial and map data

--- src/partials/map/map.js
+++ src/partials/map/map.js
@@ -1,51 +1,114 @@
-var map = L.map(document.querySelector('[data-map]'), {
-  center: [52.36663, 4.90231],
-  zoom: 12
+'use strict';
+
+// Based on http://bost.ocks.org/mike/leaflet/
+
+var prevYear = 2009;
+
+var yearControl = selectAttr('data-year-control');
+yearControl.addEventListener('click', function () {
+  var year = this.value;
+  selectAttr('data-year-control-value').value = year;
+
+  selectAttr('data-layer-year="' + prevYear + '"').classList.add('hide');
+  selectAttr('data-layer-year="' + year + '"').classList.remove('hide');
+
+  prevYear = year;
 });
 
-map.addLayer(L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}));
+if (selectAttr('data-map')) {
 
-var svg = d3.select(map.getPanes().overlayPane)
-  .append('svg');
+  var landuseCategories = {
+    onverhard: [40, 400, 411, 412, 42, 420, 431, 51, 510, 60, 600, 61, 610],
+    semiVerhard: [101, 32, 320, 33, 330, 34, 340, 35, 350, 43, 430, 44, 440, 50, 500],
+    verhard: [102, 103, 11, 110, 111, 112, 12, 120, 20, 200, 21, 210, 221, 222, 23, 230, 241, 242, 30, 300, 31, 310, 413],
+    water: [62, 620, 622, 625, 625, 73, 730, 74, 740, 75, 750, 751, 752, 753, 78, 780, 781, 782, 83, 830, 70, 700]
+  };
 
-var g = svg.append('g')
-  .attr('class', 'leaflet-zoom-hide');
+  // Convert geometry to Leaflet's geometry
+  var offset = {
+    x: 0,
+    y: 50
+  };
 
-d3.json('/assets/json-data/grondgebruik-2014.geojson', function (error, collection) {
-  if (error) throw error;
-
-  var transform = d3.geo.transform({ point: projectPoint });
-  var path = d3.geo.path().projection(transform);
-
-  var feature = g.selectAll('path')
-    .data(collection.features)
-    .enter().append('path');
-
-  map.on('viewreset', reset);
-  reset();
-
-  // Reposition the SVG to cover the features
-  function reset () {
-    var bounds = path.bounds(collection),
-      topLeft = bounds[0],
-      bottomRight = bounds[1];
-
-    svg
-      .attr('width', bottomRight[0] - topLeft[0])
-      .attr('height', bottomRight[1] - topLeft[1])
-      .style('left', topLeft[0] + 'px')
-      .style('top', topLeft[1] + 'px');
-
-    g.attr('transform', 'translate(' + -topLeft[0] + ',' + -topLeft[1] + ')');
-
-    feature.attr('d', path);
-  }
-
-  // Use Leaflet to implement a D3 geometric transformation
-  function projectPoint (x, y) {
+  var projectPoint = function (x, y) {
     var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-    this.stream.point(point.x, point.y);
+    this.stream.point(point.x + offset.x, point.y + offset.y);
+  };
+
+  // Find key name based on the array content
+  var findKey = function (object, searchValue) {
+    for (var key in object) {
+      if (~object[key].indexOf(searchValue)) {
+        return key;
+      }
+    }
+  };
+
+  // Setup map
+  var map = L.map(selectAttr('data-map'), {
+    center: [52.36663, 4.90231],
+    zoom: 15
+  });
+
+  // Setup basemap
+  map.addLayer(L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }));
+
+  var addDataLayer = function (fileName, year) {
+    var svgContainer = d3.select(map.getPanes().overlayPane)
+    .append('svg');
+
+    var dataLayer = svgContainer.append('g')
+    .attr('class', 'leaflet-data-layer leaflet-zoom-hide hide')
+    .attr('data-layer-year', year);
+
+    d3.json('/assets/json-data/' + fileName, function (error, collection) {
+      if (error) throw error;
+
+      var mapProjection = d3.geo.transform({ point: projectPoint });
+      // Generate path data according to the map projection
+      var path = d3.geo.path().projection(mapProjection);
+
+      // Create an empty path element for each feature
+      var feature = dataLayer.selectAll('path')
+        .data(collection.features)
+        .enter()
+        .append('path')
+        .attr('class', function (d) {
+          return findKey(landuseCategories, d.properties.amscode)
+        })
+        .attr('data-landusage-id', function (d) {
+          return d.properties.amscode;
+        });
+
+      map.on('viewreset', resizeSvgContainer);
+      resizeSvgContainer();
+
+      // Reposition the SVG to cover the features
+      function resizeSvgContainer () {
+        var bounds = path.bounds(collection);
+        var leftTopBounds = bounds[0];
+        var rightBottomBounds = bounds[1];
+
+        // Set the correct dimensions and position
+        svgContainer
+          .attr('width', rightBottomBounds[0] - leftTopBounds[0])
+          .attr('height', rightBottomBounds[1] - leftTopBounds[1])
+          .style('left', leftTopBounds[0] + 'px')
+          .style('top', leftTopBounds[1] + 'px');
+
+        dataLayer.attr('transform', 'translate(' +
+          -leftTopBounds[0] + ',' + -leftTopBounds[1]
+          + ')');
+
+        // Fill path with path data
+        feature.attr('d', path);
+      }
+    });
+  };
+
+  for (var year = 2009; year <= 2015; year += 1) {
+    addDataLayer('grondgebruik-' + year + '.geojson', year);
   }
-});
+}
