1efc24819e43246d302db87f957026e81163ac51
tomsnep <Snep@076.127.028.145.hva.nl>
2016-01-18 15:13:31 +0100

    project page

---
+++ src/partials/project/project.js
@@ -0,0 +1,17 @@
+var videoButton = document.querySelector('.project-video-button');
+var video = document.querySelector('.project-video');
+
+
+
+videoButton.onclick = function() {
+
+	video.classList.remove('project-video-hide');
+	video.classList.add('project-video-show');
+}
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        video.classList.remove('project-video-show');
+    }
+};
