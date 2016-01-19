fca13dff5425690aef1ce87b42d45db240509eba
tomsnep <Snep@MacBook-Pro-van-Tom.local>
2016-01-19 13:58:21 +0100

    Video + anchor tags

--- src/partials/project/project.js
+++ src/partials/project/project.js
@@ -1,14 +1,17 @@
-var videoButton = document.querySelector('.project-video-button');
-var video = document.querySelector('.project-video');
+var videoButton = document.querySelector('.project-video-button'),
+	video = document.querySelector('.project-video'),
+	videoBackground = document.querySelector('.project-container-img');
 
 videoButton.onclick = function() {
 	video.classList.remove('project-video-hide');
 	video.classList.add('project-video-show');
+	videoBackground.classList.add('project-container-img-videomodus');
 }
 
 document.onkeydown = function(evt) {
     evt = evt || window.event;
     if (evt.keyCode === 27) {
         video.classList.remove('project-video-show');
+        videoBackground.classList.remove('project-container-img-videomodus');
     }
 };
