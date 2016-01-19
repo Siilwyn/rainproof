e53346dd946133744fb5c249391eaca2a0a77253
Selwyn <talk@selwyn.cc>
2016-01-19 12:51:09 +0100

    Fix undefined video related variables

--- src/partials/project/project.js
+++ src/partials/project/project.js
@@ -1,17 +1,16 @@
-var videoButton = document.querySelector('.project-video-button');
-var video = document.querySelector('.project-video');
+if (selectAttr('project-video')) {
+	var video = selectAttr('project-video');
+	var videoButton = selectAttr('data-project-video-button');
 
+	videoButton.onclick = function () {
+		video.classList.remove('project-video-hide');
+		video.classList.add('project-video-show');
+	};
 
-
-videoButton.onclick = function() {
-
-	video.classList.remove('project-video-hide');
-	video.classList.add('project-video-show');
+	document.onkeydown = function (evt) {
+		evt = evt || window.event;
+		if (evt.keyCode === 27) {
+			video.classList.remove('project-video-show');
+		}
+	};
 }
-
-document.onkeydown = function(evt) {
-    evt = evt || window.event;
-    if (evt.keyCode == 27) {
-        video.classList.remove('project-video-show');
-    }
-};
