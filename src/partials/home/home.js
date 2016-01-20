e1e1fde6b6883d72cf4e9a7b971655d23df12375
tomsnep <Snep@MacBook-Pro-van-Tom.local>
2016-01-20 13:22:11 +0100

    home js syntax fix

--- src/partials/home/home.js
+++ src/partials/home/home.js
@@ -1,13 +1,12 @@
 if (selectAttr('data-home-text')) {
-var homeSubtext = selectAttr('data-home-subtext'),
-	homeText = selectAttr('data-home-text'),
-	homeButton1 = selectAttr('data-home-text');
+	var homeSubtext = selectAttr('data-home-subtext'),
+		homeText = selectAttr('data-home-text'),
+		homeButton1 = selectAttr('data-home-text');
 
-homeButton1.onclick = function() {
-	console.log('click works');
-	homeText.classList.add('home-text-hide');
-	homeSubtext.classList.remove('home-text-hide');
-	homeSubtext.classList.add('home-text-show');
-}
-
+	homeButton1.onclick = function() {
+		console.log('click works');
+		homeText.classList.add('home-text-hide');
+		homeSubtext.classList.remove('home-text-hide');
+		homeSubtext.classList.add('home-text-show');
+	}
 };
