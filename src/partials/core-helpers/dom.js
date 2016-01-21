594ca1e030060ca0e8c51902906c3eb17059a384
Selwyn <talk@selwyn.cc>
2016-01-21 08:51:02 +0100

    Add dist content as root

--- src/partials/core-helpers/dom.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var selectAttr = function (attrName) {
-  return document.querySelector('[' + attrName + ']');
-};
-
-var selectAttrAll = function (attrName) {
-  return document.querySelectorAll('[' + attrName + ']');
-};
